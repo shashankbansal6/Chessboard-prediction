a73ad8d3bb55264f80812b3faafd1affe2e2627b
sbansal6 <sbansal6@linux-27.ews.illinois.edu>
2018-09-08 13:04:48 -0500

    updated prediction_model

---
+++ prediction_model.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+import numpy as np
+import sklearn.model_selection import train_test_split
+rom __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import random
+
+
+def changeForm(data):
+    boards = []
+    checklist = ['r','n', 'b', 'q', 'k', 'p', 'R','N', 'B', 'Q', 'K', 'P', '.']
+    boards_final = []
+    for line in data:
+        temp = line.strip().split(" ")
+        boards.append(temp)
+
+    for i in range(len(boards))
+    return np.array(boards)
+
+def main():
+    white_data = open("white_wins.txt", 'r')
+    black_data = open("black_wins.txt", 'r')
