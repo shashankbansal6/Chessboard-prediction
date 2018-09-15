a6ece6d9648123bb6d949883e2fa4fd9df16e01a
Shashank Bansal <sbansal6@illinois.edu>
2018-09-15 18:20:57 -0500

    added more stuff

--- prediction_model.py
+++ prediction_model.py
@@ -1,13 +1,16 @@
-import tensorflow as tf
-import numpy as np
-import sklearn.model_selection import train_test_split
-rom __future__ import absolute_import
+from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+from sklearn.model_selection import train_test_split
 import random
 
-
-def changeForm(data):
+'''
+convert2list:
+    Input: data
+'''
+def covert2list(data):
     boards = []
     checklist = ['r','n', 'b', 'q', 'k', 'p', 'R','N', 'B', 'Q', 'K', 'P', '.']
     boards_final = []
@@ -22,3 +25,5 @@ def main():
     black_data = open("black_wins.txt", 'r')
 
     print(white_data)
+
+main()
